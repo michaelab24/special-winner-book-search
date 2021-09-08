7b97ec6559e3c591b855cb2b0346fdc534cf0825
michaelab24 <michaelbarnes459@yahoo.com>
2021-09-08 18:21:50 -0500

    werkin it

---
+++ server/schema/resolvers.js
@@ -0,0 +1,70 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Product, Category, Order } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, { id, username }, context) => {
+      if (context.user) {
+        const foundUser = await User.findOne({
+          $or: [{ _id: user ? user._id : id }, { username: username }],
+        });
+        return foundUser;
+      }
+
+      throw new AuthenticationError('Not logged in')
+    },
+    
+  },
+
+  Mutation: {
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      console.log(context);
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+    deleteBook: async (parent, {bookId}, context) => {
+      console.log(context);
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({
+        $or: [{ username: username }, { email: body.email }],
+      });
+      if (!user) {
+        return res.status(400).json({ message: "Can't find this user" });
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        return res.status(400).json({ message: "Wrong password!" });
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
